1160bf7502b5231591015425128acda225aaa149
sin <sin@2f30.org>
2013-11-18 21:28:46 +0000

    Set BANDCUT to 0.8

--- spectrum.c
+++ spectrum.c
@@ -156,7 +156,7 @@ draw(struct frame *fr)
 	fr->height = LINES;
 
 	/* take most of the left part of the band */
-#define BANDCUT 0.9
+#define BANDCUT 0.8
 	freqs_per_col = (nsamples / 2) / fr->width * BANDCUT;
 #undef BANDCUT
 	
