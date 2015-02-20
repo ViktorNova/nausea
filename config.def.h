6cb451abd2822f14ff681d719d165cf3ee962d76
ViktorNova <viktor@pinknoiseboys.com>
2015-02-19 19:58:22 -0800

    Better character for the falling peaks

--- config.def.h
+++ config.def.h
@@ -4,6 +4,15 @@
 #define CHPOINT L'●'
 #else
 #define CHBAR L'█'
-#define CHPEAK L'.'
+#define CHPEAK L'⎹'
+
+/* Other cool looking ones. There are lots more, see http://unicode-table.com for a full list
+#define CHPEAK L'▎'
+#define CHPEAK L'▕' 
+#define CHPEAK L'░'
+#define CHPEAK L'▒'
+
+*/
+
 #define CHPOINT L'='
 #endif
